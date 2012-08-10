78d1bbe2df0c0ac6a269b9857a409419a140baa7
guopeng.zhang <001500@archermind.com>
2012-08-10 11:48:14 +0800

    好友界面修改和归属地设置
    Change-Id: I070179283a93e4a6179c4f1c1536243e1ff288e7
    Signed-off-by: guopeng.zhang <001500@archermind.com>

---
+++ src/com/archermind/schedule/Dialog/CitySettingDialog.java
@@ -0,0 +1,224 @@
+package com.archermind.schedule.Dialog;
+
+import com.archermind.schedule.R;
+import com.archermind.schedule.Events.EventArgs;
+import com.archermind.schedule.Screens.EditScheduleScreen;
+import com.archermind.schedule.Screens.ScheduleScreen;
+
+import android.app.Dialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class CitySettingDialog implements OnClickListener{
+
+	private Dialog dialog;
+	private Button province_button;
+	private Button city_button;
+	private TextView province_text;
+	private TextView city_text;
+	private Button city_setting_ok;
+	private Button city_setting_cancel;
+	private Context context;
+	// 定义相关的字符串存放信息
+	private String provinceStr = "";
+	private String cityStr = "";
+	// xml文件中对应的省份，城市的数组
+	private int[] arrays = { R.array.beijing, R.array.shanghai,
+			R.array.tianjin, R.array.chongqing, R.array.anhui, R.array.fujian,
+			R.array.gansu, R.array.guangdong, R.array.guangxi, R.array.guizhou,
+			R.array.hainan, R.array.hebei, R.array.henan, R.array.heilongjiang,
+			R.array.hubei, R.array.hunan, R.array.jilin, R.array.jiangsu,
+			R.array.jiangxi, R.array.liaoning, R.array.neimenggu,
+			R.array.ningxia, R.array.qinghai, R.array.shandong,
+			R.array.shanxi_01, R.array.shanxi_02, R.array.sichuan,
+			R.array.xizang, R.array.xinjiang, R.array.yunnan, R.array.zhejiang, };
+	// 存放省份的数组
+	private String[] provinces;
+	String[] two_char_provinces;
+
+	int pro_position = 0;
+
+	// 定义一个数组存放arrays中对应的城市数组
+	String[] cities;
+	final int SELECT_PROVINCES = 0x111;
+	final int SELECT_OPERATOR = 0x112;
+	int cityarrayId = 0;
+	int cityId = 0;
+	boolean provinceClickOrNot = false;
+	boolean cityClickOrNot = false;
+	
+	
+	public CitySettingDialog(Context context)
+	{
+		this.context = context;
+		dialog = new Dialog(context, R.style.CustomDialog);
+		dialog.setContentView(R.layout.city_setting_dialog);
+		dialog.setCanceledOnTouchOutside(true);
+		
+		province_button = (Button)dialog.findViewById(R.id.province_button);
+		city_button = (Button)dialog.findViewById(R.id.city_button);
+		city_setting_ok = (Button)dialog.findViewById(R.id.city_setting_ok);
+		city_setting_cancel = (Button)dialog.findViewById(R.id.city_setting_cancel);
+		
+		province_text = (TextView)dialog.findViewById(R.id.province_text);
+		city_text = (TextView)dialog.findViewById(R.id.city_text);
+		
+		province_button.setOnClickListener(this);
+		city_button.setOnClickListener(this);
+		city_setting_ok.setOnClickListener(this);
+		city_setting_cancel.setOnClickListener(this);
+		
+		initPersonal();// 初始化控件
+	}
+	 
+	 public void show() {
+			dialog.show();
+		}
+	 
+	 public void dismiss() {
+		 dialog.dismiss();
+		}
+	 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId())
+		{
+		case R.id.province_button:
+			showDialog();
+			break;
+		case R.id.city_button:
+			showDialog(cities, true);
+			break;
+		case R.id.city_setting_ok:
+			break;
+		case R.id.city_setting_cancel:
+			break;
+		}
+	}
+	
+	private void initPersonal() {
+		provinces = context.getResources().getStringArray(R.array.provinces);
+		two_char_provinces = context.getResources().getStringArray(
+				R.array.two_chars_provinces);
+		provinces = context.getResources().getStringArray(R.array.provinces);
+		cities = (String[]) context.getResources().getStringArray(arrays[pro_position]);
+
+	}
+	
+	private void showDialog() {
+
+			Builder builder = new android.app.AlertDialog.Builder(context);
+			// 设置对话框的标题
+			builder.setTitle("选择省份");
+			// 0: 默认第一个单选按钮被选中
+			builder.setSingleChoiceItems(R.array.provinces,
+					getProvincesId(province_text.getText().toString()),
+					new DialogInterface.OnClickListener() {
+
+						public void onClick(DialogInterface dialog, int which) {
+
+							provinceStr = context.getResources().getStringArray(
+									R.array.provinces)[which];
+
+							cityarrayId = which;
+
+							provinceClickOrNot = true;
+						}
+					});
+			// 确定按钮
+			builder.setPositiveButton("确定",
+					new DialogInterface.OnClickListener() {
+
+						public void onClick(DialogInterface dialog, int which) {
+
+							if (provinceClickOrNot) {
+								provinceStr = context.getResources().getStringArray(
+										R.array.provinces)[cityarrayId];
+								province_text.setText(provinceStr);
+								cities = (String[]) context.getResources()
+										.getStringArray(arrays[cityarrayId]);
+								cityStr = context.getResources().getStringArray(
+										arrays[cityarrayId])[0];
+								city_text.setText(cityStr);
+							} else {
+
+								pro_position = getProvincesId(province_text
+										.getText().toString());
+
+								provinceStr = context.getResources().getStringArray(
+										R.array.provinces)[pro_position];
+								cities = (String[]) context.getResources()
+										.getStringArray(arrays[pro_position]);
+								cityStr = context.getResources().getStringArray(
+										arrays[pro_position])[0];
+								province_text.setText(provinceStr);
+								city_text.setText(cityStr);
+							}
+						}
+					}).show();
+	}
+
+	private void showDialog(final String[] array, final boolean city) {
+		Builder builder = new android.app.AlertDialog.Builder(context);
+		// 设置对话框的标题
+
+		// 0: 默认第一个单选按钮被选中
+		if (city) {
+			builder.setTitle(province_text.getText().toString());
+			String[] pro_city = array;
+			for (int i = 0; i < pro_city.length; i++) {
+				if (cityStr.equals(pro_city[i])) {
+					cityId = i;
+				}
+			}
+			builder.setSingleChoiceItems(array, cityId,
+					new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							cityStr = array[which];
+							cityClickOrNot = true;
+						}
+					});
+		}
+		// 确定按钮
+		builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+			public void onClick(DialogInterface dialog, int which) {
+				if (city) {
+					if (cityClickOrNot) {
+						city_text.setText(cityStr);
+					} else {
+						cityStr = context.getResources().getStringArray(
+								arrays[getProvincesId(province_text
+										.getText().toString())])[cityId];
+						city_text.setText(cityStr);
+					}
+				}
+			}
+		}).show();
+	}
+	
+	private int getProvincesId(String string) {
+		// Log.i("my", "String==========" + string);
+		int pro_position = 0;
+		for (int i = 0; i < provinces.length; i++) {
+			if (string.equals(provinces[i])) {
+				pro_position = i;
+			}
+		}
+		return pro_position;
+	}
+
+}
