57e6374d3110a4093e70a8ad7bce5269b4419e28
guopeng.zhang <001500@archermind.com>
2012-10-17 09:37:16 +0800

    对日志的封装
    Signed-off-by: guopeng.zhang <001500@archermind.com>

--- src/com/archermind/schedule/ScheduleApplication.java
+++ src/com/archermind/schedule/ScheduleApplication.java
@@ -22,8 +22,43 @@ public class ScheduleApplication extends Application {
 		super.onCreate();
 	}
 
-	@SuppressWarnings("rawtypes")
 	public static void LogD(Class classz, String str) {
 		Log.d(TAG, classz.getCanonicalName() + "--->" + str);
+	}
+
+	public static void LogI(Class classz, String str) {
+		Log.i(TAG, classz.getCanonicalName() + "--->" + str);
+	}
+
+	public static void LogE(Class classz, String str) {
+		Log.e(TAG, classz.getCanonicalName() + "--->" + str);
+	}
+
+	public static void LogV(Class classz, String str) {
+		Log.v(TAG, classz.getCanonicalName() + "--->" + str);
+	}
+
+	public static void logException(Class c, Throwable e) {
+		try {
+			StringBuilder exceptionInfo = new StringBuilder();
+			if (e == null) {
+				exceptionInfo.append("Exception:"
+						+ "e is null,probably null pointer exception" + "\n");
+			} else {
+				e.printStackTrace();
+				exceptionInfo.append(e.getClass().getCanonicalName() + ":"
+						+ e.getMessage() + "\n");
+				StackTraceElement[] stes = e.getStackTrace();
+				for (StackTraceElement ste : stes) {
+					exceptionInfo.append("at " + ste.getClassName() + "$"
+							+ ste.getMethodName() + "$" + ste.getFileName()
+							+ ":" + ste.getLineNumber() + "\n");
+				}
+			}
+
+			LogE(c, exceptionInfo.toString());
+		} catch (Exception ex) {
+			LogE(c, ex.getMessage());
+		}
 	}
 }
